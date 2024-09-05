8bb80e3a3a9c02312a33133c473eba4dccd7f808
wurenand <2780049236@QQ.COM>
2024-09-05 17:31:03 +0800

    创建角色基类，添加武器插槽，武器网格体，创建角色和敌人子类

---
+++ Source/DemoGAS/Private/Character/DemoCharacterBase.cpp
@@ -0,0 +1,19 @@
+﻿
+#include "DemoGAS/Public/Character/DemoCharacterBase.h"
+
+
+ADemoCharacterBase::ADemoCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	//Weapon初始化
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(GetMesh(),FName(TEXT("WeaponHandSocket")));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
+}
+
+void ADemoCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/DemoGAS/Public/Character/DemoCharacterBase.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DemoCharacterBase.generated.h"
+
+//Abstract修饰符标识这个类不能被实例
+UCLASS(Abstract)
+class DEMOGAS_API ADemoCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ADemoCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	/*使用TObjectPtr的优点:在打包后会被解释为原始指针，与原始指针无异
+	 *但在编辑器中，提供了
+	 *1.访问追踪：跟踪指针被访问或者取消引用的频率
+	 *2.Lazy Load:只在需要的时候加载资产
+	 */
+	UPROPERTY(EditAnywhere,Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
+	
+};
