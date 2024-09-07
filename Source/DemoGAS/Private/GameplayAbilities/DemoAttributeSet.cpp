05276da9779eca56a6e6d8c0f8acfba083fa93af
wurenand <2780049236@QQ.COM>
2024-09-07 11:04:08 +0800

    新增- 在AttributeSet中添加了4个AttributeData。 明确了新增AttributeData的流程： 1.生命FGameplayAttributeData变量，标记为ReplicatedUsing 2.利用ATTRIBUTE_ACCESSORS宏，创建访问函数 3.在GetLifetimeReplicatedProps中注册复制变量 4.创建相应的OnRep函数，并在函数中调用宏GAMEPLAYATTRIBUTE_REPNOTIFY来通知ASC完成了属性复制

--- Source/DemoGAS/Private/GameplayAbilities/DemoAttributeSet.cpp
+++ Source/DemoGAS/Private/GameplayAbilities/DemoAttributeSet.cpp
@@ -2,3 +2,45 @@
 
 
 #include "GameplayAbilities/DemoAttributeSet.h"
+
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UDemoAttributeSet::UDemoAttributeSet()
+{
+	//初始化Attribute
+	InitHealth(100);
+	InitMaxHealth(100);
+	InitMana(100);
+	InitMaxMana(100);
+}
+
+void UDemoAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDemoAttributeSet,Health,COND_None,REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UDemoAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDemoAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDemoAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+
+void UDemoAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue) const
+{
+	//向ASC通知属性完成了复制，TODO(用于预测回滚等?)
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDemoAttributeSet,Health,OldValue)
+}
+
+void UDemoAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDemoAttributeSet,MaxHealth,OldValue); 
+}
+
+void UDemoAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDemoAttributeSet,Mana,OldValue);
+}
+
+void UDemoAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDemoAttributeSet,MaxMana,OldValue);
+}
--- Source/DemoGAS/Public/GameplayAbilities/DemoAttributeSet.h
+++ Source/DemoGAS/Public/GameplayAbilities/DemoAttributeSet.h
@@ -3,10 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 
 #include "DemoAttributeSet.generated.h"
 
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -14,4 +20,32 @@ UCLASS()
 class DEMOGAS_API UDemoAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	//在运行时使用 ~showdebug abilitysystem可以方便调试
+	UDemoAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attribute")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UDemoAttributeSet,Health)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attribute")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UDemoAttributeSet,MaxHealth)
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana,Category = "Vital Attribute")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UDemoAttributeSet,Mana)
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana,Category = "Vital Attribute")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UDemoAttributeSet,MaxMana)
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue) const;
 };
