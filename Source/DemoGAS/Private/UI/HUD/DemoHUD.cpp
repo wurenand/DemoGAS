198d026a123e367c4d81eec4d47da9266123ef4e
wurenand <2780049236@QQ.COM>
2024-09-08 11:17:10 +0800

    新增- 利用DemoUserWidget类创建了子类WBP_GlobeProgressBar。并设定了基本参数的函数。 再基于它创建了Health和Mana的WBP。 创建了WBP_Overlay作为主UI，并通过DemoHUD添加到视口

---
+++ Source/DemoGAS/Private/UI/HUD/DemoHUD.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/DemoHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/DemoUserWidget.h"
+
+void ADemoHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//创建OverlayWidget
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+
+	OverlayWidget = Cast<UDemoUserWidget>(Widget);
+}
---
+++ Source/DemoGAS/Public/UI/HUD/DemoHUD.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DemoHUD.generated.h"
+
+class UDemoUserWidget;
+/**
+ * 创建管理OverlayWidget（MainUI）
+ */
+UCLASS()
+class DEMOGAS_API ADemoHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TObjectPtr<UDemoUserWidget> OverlayWidget;
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> OverlayWidgetClass;
+};
