12f02d35e84848f38d418b230664ec8b518c1658
wurenand <2780049236@QQ.COM>
2024-09-18 15:02:41 +0800

    新增- 创建DataAsset：DemoInputConfig， 仿制lyra利用GameplayTag映射InputAction。实现通过Tag获取IA，动态绑定IA对应函数？

---
+++ Source/DemoGAS/Private/Input/DemoInputConfig.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/DemoInputConfig.h"
+
+const UInputAction* UDemoInputConfig::FindInputActionFromTag(const FGameplayTag& Tag, bool bLogNotFound)
+{
+	for(const FDemoInputAction& DemoInputAction : AbilityInputActions)
+	{
+		if(DemoInputAction.InputAction && DemoInputAction.InputTag == Tag)
+		{
+			return DemoInputAction.InputAction;
+		}
+	}
+	
+	if(bLogNotFound)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Tag[%s] Not Found In InputConfig"), *Tag.GetTagName().ToString());
+	}
+	return nullptr;
+}
---
+++ Source/DemoGAS/Public/Input/DemoInputConfig.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "DemoInputConfig.generated.h"
+
+class UInputAction;
+
+/**
+ * 自定义的输入操作的结构
+ */
+USTRUCT(BlueprintType)
+struct FDemoInputAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	const UInputAction* InputAction = nullptr;
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag = FGameplayTag();
+	
+};
+
+/**
+ * 仿制Lyra
+ */
+UCLASS()
+class DEMOGAS_API UDemoInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 返回根据Tag的对应InputAction
+	 * @param Tag 需要寻找的Tag
+	 * @param bLogNotFound true则会在没找到的时候记录UE_LOG
+	 * @return 未找到就会返回nullptr
+	 */
+	UFUNCTION(BlueprintCallable)
+	const UInputAction* FindInputActionFromTag(const FGameplayTag& Tag,bool bLogNotFound);
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	TArray<FDemoInputAction> AbilityInputActions;
+	
+};
