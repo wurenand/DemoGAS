afe3a87445a1e6bf0909a9ef2bf83f2e13c95a61
wurenand <2780049236@QQ.COM>
2024-09-27 14:53:36 +0800

    重构- 增加新基类AInteractBaseActor作为DemoEffectActor和DemoProjectile的基类。提供ETeam，TargetActor变量，作为交互时的依据。

--- Source/DemoGAS/Public/Actor/DemoEffectActor.h
+++ Source/DemoGAS/Public/Actor/DemoEffectActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "ActiveGameplayEffectHandle.h"
+#include "InteractBaseActor.h"
 #include "GameFramework/Actor.h"
 #include "DemoEffectActor.generated.h"
 
@@ -35,13 +36,13 @@ class UGameplayEffect;
 //视觉效果Mesh和碰撞体Component由蓝图实现。
 //C++只负责处理碰撞后的逻辑
 UCLASS()
-class DEMOGAS_API ADemoEffectActor : public AActor
+class DEMOGAS_API ADemoEffectActor : public AInteractBaseActor
 {
 	GENERATED_BODY()
 
 public:
 	ADemoEffectActor();
-	
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -51,34 +52,34 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void OnEndOverlap(AActor* TargetActor);
 	//~end
-	
+
 	UFUNCTION(BlueprintCallable)
-	void ApplyGameplayEffectToTarget(AActor* TargetActor,TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyGameplayEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Setting")
-	bool bDestroyAfterGE = false;//GE被移除后是否摧毁Actor （暂时没有实现）
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Setting")
+	bool bDestroyAfterGE = false; //GE被移除后是否摧毁Actor （暂时没有实现）
 
-	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category = "GAS|Effects|Setting")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "GAS|Effects|Setting")
 	float EffectLevel = 1.f;
-	
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Class")
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Class")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Policy")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Policy")
 	EEffectApplicationPolicy InstantGEApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Class")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Class")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Policy")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Policy")
 	EEffectApplicationPolicy DurationGEApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
-	
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Class")
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Class")
 	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Policy")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Policy")
 	EEffectApplicationPolicy InfiniteGEApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
-	UPROPERTY(BlueprintReadOnly,EditAnywhere,Category = "GAS|Effects|Policy")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Effects|Policy")
 	EEffectRemovePolicy InfiniteGERemovePolicy = EEffectRemovePolicy::RemoveOnEndOverlap;
-	
+
 private:
 	//存储每个Actor的ASC到对应的Active的InfiniteGE的映射，用于在ActorEndOverlap之后remove这个InfiniteGE
-	TMap<UAbilitySystemComponent*,FActiveGameplayEffectHandle> InfiniteOverlapActors;
+	TMap<UAbilitySystemComponent*, FActiveGameplayEffectHandle> InfiniteOverlapActors;
 };
--- Source/DemoGAS/Public/Actor/DemoProjectile.h
+++ Source/DemoGAS/Public/Actor/DemoProjectile.h
@@ -1,31 +1,35 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "InteractBaseActor.h"
 #include "GameFramework/Actor.h"
 #include "DemoProjectile.generated.h"
 
+class UNiagaraSystem;
 class UProjectileMovementComponent;
 class USphereComponent;
 
 UCLASS()
-class DEMOGAS_API ADemoProjectile : public AActor
+class DEMOGAS_API ADemoProjectile : public AInteractBaseActor
 {
 	GENERATED_BODY()
 
 public:
 	ADemoProjectile();
 
-	UPROPERTY(VisibleAnywhere,Category = "Movement")
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
-	
+
 protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION()
-	void OnSphereOverlappedBegin(UPrimitiveComponent* OverlappedComponent,AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,int32 OtherBodyIndex,bool bFromSweep,const FHitResult& SweepResult);
+	void OnSphereOverlappedBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                             const FHitResult& SweepResult);
 
 private:
-	UPROPERTY(VisibleAnywhere,Category = "Collision")
+	UPROPERTY(VisibleAnywhere, Category = "Collision")
 	TObjectPtr<USphereComponent> Sphere;
+
 };
