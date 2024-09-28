fb9fb370144f53e7c231d678a96390a61722dc03
wurenand <2780049236@QQ.COM>
2024-09-28 17:55:55 +0800

    新增- 创建DataAsset：UCharacterClassInfo来作为不同类型Character获取数据（GA，GE初始化数据等） 的来源

---
+++ Source/DemoGAS/Private/GameplayAbilities/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbilities/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInfomation.FindChecked(CharacterClass);
+}
---
+++ Source/DemoGAS/Public/GameplayAbilities/Data/CharacterClassInfo.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	//TODO:这里选择不同英雄
+	ECC_Ezreal UMETA(DisplayName = "伊泽瑞尔"),
+	ECC_JINZHAN  UMETA(DisplayName = "伊泽瑞尔2"),
+	ECC_TEST2  UMETA(DisplayName = "伊泽瑞尔3"),
+};
+
+/**
+ * 每个Character独享的部分
+ */
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,Category = "ClassDefaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributesEffectClass;	
+};
+
+/**
+ * 英雄的信息，包括GA，数值表，GE
+ */
+//TODO:是不是还应该有GA呢？
+UCLASS()
+class DEMOGAS_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	//所有数据
+	UPROPERTY(EditDefaultsOnly,Category = "CharacterClassDefaults")
+	TMap<ECharacterClass,FCharacterClassDefaultInfo> CharacterClassInfomation;
+	
+	//每种英雄共享的部分
+	UPROPERTY(EditDefaultsOnly,Category = "CommonClassDefaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributesEffectClass;
+	UPROPERTY(EditDefaultsOnly,Category = "CommonClassDefaults")
+	TSubclassOf<UGameplayEffect> VitalAttributesEffectClass;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+};
