d306a48886c06daf3fa99fee089da12591de3d7e
wurenand <2780049236@qq.com>
2024-10-11 01:20:27 +0800

    新增- 了解了FGameplayEffectContext的成员变量序列化

---
+++ Source/DemoGAS/Private/DemoAbilitySystemTypes.cpp
@@ -0,0 +1,103 @@
+﻿#include "DemoAbilitySystemTypes.h"
+
+
+bool FDemoGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+{
+	//每一位代表需要序列化的成员
+	uint32 RepBits = 0;
+
+	//标识需要序列化的成员
+	if (Ar.IsSaving())
+	{
+		if (bReplicateInstigator && Instigator.IsValid())
+		{
+			RepBits |= 1 << 0;
+		}
+		if (bReplicateEffectCauser && EffectCauser.IsValid())
+		{
+			RepBits |= 1 << 1;
+		}
+		if (AbilityCDO.IsValid())
+		{
+			RepBits |= 1 << 2;
+		}
+		if (bReplicateSourceObject && SourceObject.IsValid())
+		{
+			RepBits |= 1 << 3;
+		}
+		if (Actors.Num() > 0)
+		{
+			RepBits |= 1 << 4;
+		}
+		if (HitResult.IsValid())
+		{
+			RepBits |= 1 << 5;
+		}
+		if (bHasWorldOrigin)
+		{
+			RepBits |= 1 << 6;
+		}
+		if (bIsCriticalHit)
+		{
+			RepBits |= 1 << 7;
+		}
+	}
+
+	//序列化 （能够让反序列化过程知道哪些成员包含在数据中）
+	Ar.SerializeBits(&RepBits, 8);
+
+	//实际序列化/反序列化
+	if (RepBits & (1 << 0))
+	{
+		Ar << Instigator;
+	}
+	if (RepBits & (1 << 1))
+	{
+		Ar << EffectCauser;
+	}
+	if (RepBits & (1 << 2))
+	{
+		Ar << AbilityCDO;
+	}
+	if (RepBits & (1 << 3))
+	{
+		Ar << SourceObject;
+	}
+	if (RepBits & (1 << 4))
+	{
+		SafeNetSerializeTArray_Default<31>(Ar, Actors);
+	}
+	if (RepBits & (1 << 5))
+	{
+		if (Ar.IsLoading())
+		{
+			if (!HitResult.IsValid())
+			{
+				HitResult = MakeShared<FHitResult>();
+			}
+		}
+		HitResult->NetSerialize(Ar, Map, bOutSuccess);
+	}
+	if (RepBits & (1 << 6))
+	{
+		Ar << WorldOrigin;
+		bHasWorldOrigin = true;
+	}
+	else
+	{
+		bHasWorldOrigin = false;
+	}
+	if (RepBits & (1 << 7))
+	{
+		Ar << bIsCriticalHit;
+	}
+
+	//反序列化后的初始化Context数据
+	if (Ar.IsLoading())
+	{
+		AddInstigator(Instigator.Get(), EffectCauser.Get()); // Just to initialize InstigatorAbilitySystemComponent
+	}
+
+	bOutSuccess = true;
+	return true;
+}
---
+++ Source/DemoGAS/Public/DemoAbilitySystemTypes.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+
+#include "DemoAbilitySystemTypes.generated.h"
+/**
+ * 自定义子类EffectContext
+ * 传递一些信息
+ * 子类必须要：1 GetScriptStruct 2 NetSerialize 3 TStructOpsTypeTraits<FDemoGameplayEffectContext>
+ */
+USTRUCT(BlueprintType)
+struct FDemoGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+	
+public:
+	bool IsCriticalHit() const {return bIsCriticalHit;};
+	void SetIsCriticalHit(bool bInCriticalHit) {bIsCriticalHit = bInCriticalHit;}; 
+	
+	//子类必须重写 （类似UClass的GetClass函数）
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::GetScriptStruct();
+	}
+
+	
+	/**
+	 * 
+	 * @param Ar 能够序列化和解析序列化的成员变量
+	 * @param Map 通过object或name映射到索引或从索引映射 （用于解析序列化后的数据具体的位置？）
+	 * @param bOutSuccess 
+	 * @return 
+	 */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+	bool bIsCriticalHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits<FDemoGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FDemoGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerialize = true,
+		WithCopy = true,
+	};
+};
