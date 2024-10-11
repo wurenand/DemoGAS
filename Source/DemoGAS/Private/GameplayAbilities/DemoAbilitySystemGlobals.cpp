ed4a7bee96c9acc8960df18dccf56a9f8027a9b2
wurenand <2780049236@qq.com>
2024-10-11 17:37:45 +0800

    新增- 为了使用自定义的GEContext，需要自定义AbilitySystemGlobals并在config文件中配置 问题！！单人运行正常，多人会崩溃？？？？？？？

---
+++ Source/DemoGAS/Private/GameplayAbilities/DemoAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbilities/DemoAbilitySystemGlobals.h"
+
+#include "DemoAbilitySystemTypes.h"
+
+FGameplayEffectContext* UDemoAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FDemoGameplayEffectContext();
+}
---
+++ Source/DemoGAS/Public/GameplayAbilities/DemoAbilitySystemGlobals.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "DemoAbilitySystemGlobals.generated.h"
+
+/**
+ * 创建这个类的子类来自定义一些类 （FGameplayEffectContext）
+ */
+UCLASS()
+class DEMOGAS_API UDemoAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+
+	//TODO: 单人运行正常，多人会崩溃？？？？？？？
+public:
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+};
