8c42b77b0461647d77ec88ce66013922bdda6058
wurenand <2780049236@qq.com>
2024-10-15 16:00:28 +0800

    新增- 成功导入第一个LOL角色，通过解包LOL文件获取模型动作。通过动画蓝图模板实现动画蓝图逻辑复用

---
+++ Source/DemoGAS/Private/GameplayAbilities/Abilities/Normal/NormalAbilityBase.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbilities/Abilities/Normal/NormalAbilityBase.h"
+
+#include "GameplayTagsManager.h"
+
+UNormalAbilityBase::UNormalAbilityBase()
+{
+	TriggerInputTag = UGameplayTagsManager::Get().RequestGameplayTag("InputAction.RMB");
+}
---
+++ Source/DemoGAS/Public/GameplayAbilities/Abilities/Normal/NormalAbilityBase.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/Abilities/DamageGameplayAbility.h"
+#include "NormalAbilityBase.generated.h"
+
+/**
+ * 普通攻击的GA的基类
+ * 包括 攻击距离检测，InputTag
+ */
+UCLASS()
+class DEMOGAS_API UNormalAbilityBase : public UDamageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UNormalAbilityBase();
+};
