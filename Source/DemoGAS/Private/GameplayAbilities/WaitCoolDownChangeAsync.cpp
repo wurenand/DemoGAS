bec50c287e40c35ad5547c5aca1dcb55e5c579a0
wurenand <2780049236@qq.com>
2024-10-20 19:03:02 +0800

    新增- 利用Async节点实现了GA的冷却时间显示

---
+++ Source/DemoGAS/Private/GameplayAbilities/WaitCoolDownChangeAsync.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbilities/WaitCoolDownChangeAsync.h"
+
+#include "AbilitySystemComponent.h"
+
+UWaitCoolDownChangeAsync* UWaitCoolDownChangeAsync::WaitCoolDownChange(UAbilitySystemComponent* InASC,
+                                                                       const FGameplayTag& InCoolDownTag)
+{
+	UWaitCoolDownChangeAsync* WaitCoolDownChangeAsync = NewObject<UWaitCoolDownChangeAsync>();
+	WaitCoolDownChangeAsync->ASC = InASC;
+	WaitCoolDownChangeAsync->CoolDownTag = InCoolDownTag;
+	if (!IsValid(InASC) || !InCoolDownTag.IsValid())
+	{
+		WaitCoolDownChangeAsync->EndTask();
+		return nullptr;
+	}
+
+	InASC->RegisterGameplayTagEvent(InCoolDownTag, EGameplayTagEventType::NewOrRemoved)
+	     .AddUObject(WaitCoolDownChangeAsync, &UWaitCoolDownChangeAsync::CoolDownChanged);
+
+	//在Server或者Client如果Apply了DurationGE就会广播
+	InASC->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCoolDownChangeAsync,
+	                                                            &UWaitCoolDownChangeAsync::GameplayEffectAppliedToSelf);
+
+	return WaitCoolDownChangeAsync;
+}
+
+void UWaitCoolDownChangeAsync::EndTask()
+{
+	if (!IsValid(ASC))
+	{
+		return;
+	}
+	ASC->RegisterGameplayTagEvent(CoolDownTag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCoolDownChangeAsync::CoolDownChanged(const FGameplayTag InCoolDownTag, int NewCount)
+{
+	if (NewCount == 0)
+	{
+		OnCoolDownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCoolDownChangeAsync::GameplayEffectAppliedToSelf(UAbilitySystemComponent* TargetASC,
+                                                           const FGameplayEffectSpec& EffectSpec,
+                                                           FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	EffectSpec.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	EffectSpec.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CoolDownTag) || GrantedTags.HasTagExact(CoolDownTag))
+	{
+		//OwningTagsQuery = AssetTags + GrantedTags + DynamicGrantedTags
+		FGameplayEffectQuery Query = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(
+			CoolDownTag.GetSingleTagContainer());
+		OnCoolDownStart.Broadcast(ASC->GetActiveEffectsTimeRemaining(Query)[0]);
+	}
+}
---
+++ Source/DemoGAS/Public/GameplayAbilities/WaitCoolDownChangeAsync.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCoolDownChangeAsync.generated.h"
+
+struct FActiveGameplayEffectHandle;
+struct FGameplayEffectSpec;
+class UAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCoolDownChangeDelegate, float, CoolDownRemaining);
+
+/**
+ * 异步节点 广播GA的CoolDown
+ */
+UCLASS()
+class DEMOGAS_API UWaitCoolDownChangeAsync : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	public:
+	UPROPERTY(BlueprintAssignable)
+	FOnCoolDownChangeDelegate OnCoolDownStart;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnCoolDownChangeDelegate OnCoolDownEnd;
+
+	UFUNCTION(BlueprintCallable,meta=(BlueprintInternalUseOnly = "true"))
+	static UWaitCoolDownChangeAsync* WaitCoolDownChange(UAbilitySystemComponent* InASC,const FGameplayTag& InCoolDownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();//回收资源
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	FGameplayTag CoolDownTag;
+
+	void CoolDownChanged(const FGameplayTag InCoolDownTag,int NewCount);
+	//绑定到DurationGE被Apply之后的回调 用于得到这个GE的Duration时长
+	void GameplayEffectAppliedToSelf(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& EffectSpec,FActiveGameplayEffectHandle ActiveGameplayEffectHandle);
+};
