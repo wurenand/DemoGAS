3f99dca25b64406e18c226734fd641abeb242e36
wurenand <2780049236@qq.com>
2024-10-30 12:34:11 +0800

    修复- 通过判断IsInSeamlessTravel来解决上一次提交的直接进入游戏地图不能正确生成Pawn的问题

--- Source/DemoGAS/Private/Game/DemoGameModeBase.cpp
+++ Source/DemoGAS/Private/Game/DemoGameModeBase.cpp
@@ -9,21 +9,23 @@
 
 void ADemoGameModeBase::RestartPlayer(AController* NewPlayer)
 {
-	if (ADemoPlayerController* NewPlayerController = Cast<ADemoPlayerController>(NewPlayer))
+	if(GetWorld()->IsInSeamlessTravel())
 	{
-		if (ADemoPlayerState* DemoPlayerState = NewPlayerController->GetPlayerState<ADemoPlayerState>())
+		if (ADemoPlayerController* NewPlayerController = Cast<ADemoPlayerController>(NewPlayer))
 		{
-			FCharacterClassDefaultInfo CharacterClassDefaultInfo = CharacterClassInfo->GetClassDefaultInfo(
-				DemoPlayerState->CharacterClass);
-			checkf(CharacterClassDefaultInfo.PlayerCharacterBPClass,
-			       TEXT("CharacterClassDefaultInfo.PlayerCharacterBPClass is null"));
-			TSubclassOf<ADemoPlayerCharacter> SpawnClass = CharacterClassDefaultInfo.PlayerCharacterBPClass;
-			FTransform SpawnTransform;
-			FActorSpawnParameters SpawnParameters;
-			ADemoPlayerCharacter* SpawnCharacter = GetWorld()->SpawnActor<ADemoPlayerCharacter>(SpawnClass,SpawnTransform,SpawnParameters);
-			NewPlayerController->SetPawn(SpawnCharacter);
-			//TODO:从别的地图传送过来可以正常SpawnPawn，但是直接运行游戏地图却不能正常SpawnPawn
-			FinishRestartPlayer(NewPlayerController,FRotator(0.0f));
+			if (ADemoPlayerState* DemoPlayerState = NewPlayerController->GetPlayerState<ADemoPlayerState>())
+			{
+				FCharacterClassDefaultInfo CharacterClassDefaultInfo = CharacterClassInfo->GetClassDefaultInfo(
+					DemoPlayerState->CharacterClass);
+				checkf(CharacterClassDefaultInfo.PlayerCharacterBPClass,
+					   TEXT("CharacterClassDefaultInfo.PlayerCharacterBPClass is null"));
+				TSubclassOf<ADemoPlayerCharacter> SpawnClass = CharacterClassDefaultInfo.PlayerCharacterBPClass;
+				FTransform SpawnTransform;
+				FActorSpawnParameters SpawnParameters;
+				ADemoPlayerCharacter* SpawnCharacter = GetWorld()->SpawnActor<ADemoPlayerCharacter>(SpawnClass,SpawnTransform,SpawnParameters);
+				NewPlayerController->SetPawn(SpawnCharacter);
+				FinishRestartPlayer(NewPlayerController,FRotator(0.0f));
+			}
 		}
 	}
 	else
