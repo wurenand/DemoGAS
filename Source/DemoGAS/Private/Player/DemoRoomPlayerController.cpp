5b642627b202620a9cb4fcf603dbd0f6c75038ab
wurenand <2780049236@qq.com>
2024-11-05 18:21:19 +0800

    新增- 创建Room关卡的PC，用于管理RoomUI

---
+++ Source/DemoGAS/Private/Player/DemoRoomPlayerController.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/DemoRoomPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+
+void ADemoRoomPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	checkf(RoomWidgetClass,TEXT("RoomWidgetClass is NULL"));
+	RoomWidget = CreateWidget(this,RoomWidgetClass);
+	bShowMouseCursor = true;
+	FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+	RoomWidget->AddToViewport(0);
+}
---
+++ Source/DemoGAS/Public/Player/DemoRoomPlayerController.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DemoRoomPlayerController.generated.h"
+
+/**
+ * 作为Room关卡中的PC，控制RoomUI
+ */
+UCLASS()
+class DEMOGAS_API ADemoRoomPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void BeginPlay() override;
+	protected:
+	UPROPERTY(EditAnywhere,Category = "RoomUI")
+	TSubclassOf<UUserWidget> RoomWidgetClass;
+	UPROPERTY(EditAnywhere,Category = "RoomUI")
+	TObjectPtr<UUserWidget> RoomWidget;
+	
+};
